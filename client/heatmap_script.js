3a3ef49a846e86f5dff18f85d13cdca74a7cb5d6
ttormo <ttormo@umd.edu>
2022-12-07 21:45:23 -0500

    leaflet

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -1,6 +1,6 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+const map = L.map('map').setView([38.9897, -76.9378], 11);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+}
