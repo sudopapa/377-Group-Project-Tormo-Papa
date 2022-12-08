86983d4ef4557b2a4faf93b85f6e7f219fd1a161
ttormo <ttormo@umd.edu>
2022-12-07 21:38:14 -0500

    decided to just get leaflet working first before trying to add in the heatmap

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -1,43 +1,6 @@
-// don't forget to include leaflet-heatmap.js
-var testData = {
-max: 8,
-data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, ...]
-};
+const map = L.map('map').setView([51.505, -0.09], 13);
 
-var baseLayer = L.tileLayer(
-'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-attribution: '...',
-maxZoom: 18
-}
-);
-
-var cfg = {
-// radius should be small ONLY if scaleRadius is true (or small radius is intended)
-// if scaleRadius is false it will be the constant radius used in pixels
-"radius": 2,
-"maxOpacity": .8,
-// scales the radius based on map zoom
-"scaleRadius": true,
-// if set to false the heatmap uses the global maximum for colorization
-// if activated: uses the data maximum within the current map boundaries
-//   (there will always be a red spot with useLocalExtremas true)
-"useLocalExtrema": true,
-// which field name in your data represents the latitude - default "lat"
-latField: 'lat',
-// which field name in your data represents the longitude - default "lng"
-lngField: 'lng',
-// which field name in your data represents the data value - default "value"
-valueField: 'count'
-};
-
-
-var heatmapLayer = new HeatmapOverlay(cfg);
-
-var map = new L.Map('map-canvas', {
-center: new L.LatLng(25.6586, -80.3568),
-zoom: 4,
-layers: [baseLayer, heatmapLayer]
-});
-
-heatmapLayer.setData(testData);
-
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
