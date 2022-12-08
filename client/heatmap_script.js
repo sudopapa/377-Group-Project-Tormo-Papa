083d2ba47e85dd201c063cc605d18f2579bbe87b
ttormo <ttormo@umd.edu>
2022-12-07 22:35:33 -0500

    reverting back to normal leaflet without heatmap to fix

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -1,23 +1,25 @@
 // Initial Leaflet Map
-// const map = L.map('map').setView([38.9897, -76.9378], 11);
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   maxZoom: 19,
-//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+const map = L.map('map').setView([38.9897, -76.9378], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 
 // Leaflet Map w/Heatmap Code
+/*
 var testData = {
     max: 8,
     data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, ...]
   };
-  
+
   var baseLayer = L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
       attribution: '...',
       maxZoom: 18
     }
   );
-  
+
   var cfg = {
     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
     // if scaleRadius is false it will be the constant radius used in pixels
@@ -36,14 +38,15 @@ var testData = {
     // which field name in your data represents the data value - default "value"
     valueField: 'count'
   };
-  
-  
+
   var heatmapLayer = new HeatmapOverlay(cfg);
-  
+
   var map = new L.Map('map-canvas', {
     center: new L.LatLng(25.6586, -80.3568),
     zoom: 4,
     layers: [baseLayer, heatmapLayer]
   });
-  
+
   heatmapLayer.setData(testData);
+
+*/
