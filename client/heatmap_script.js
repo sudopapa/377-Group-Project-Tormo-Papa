adc57db31ed2247d3e2c1c144a39a874bbc56c87
ttormo <ttormo@umd.edu>
2022-12-12 17:05:00 -0500

    re-tooling the entire project to focus around a new API, specifically a new implementation of the PG County one

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -2,78 +2,94 @@
 import { tileLayer, Map, LatLng } from 'leaflet';
 import HeatmapOverlay from 'leaflet-heatmap';
 
-// API Module
-const APIController = (function() {
-  // API Methods
-  const _getToken = async () => {
-    const accessToken = 'BQACX0bVaTJ99cc7DJwDua1RqDnpj-VXOPacGoI51GypyRWNwCS2wJdlkd--TIFoVXpSNCppN1D_Xv4RvJhu8d9gHzwVOVyuzgdpzaAw96yjWAOTtXtvnQDcj8TrHXq3aIKxbgPSbIdnJJDz74jcb0lNbQztUxoemvnvdyZ0DtwyzO8QAuNXlNiMgfUNaFFm7XU';
-    return accessToken;
-  };
-
-  const _getGenres = async (token) => {
-    const limit = 10;
-
-    const result = await fetch('https://api.spotify.com/v1/browse/categories', {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${token}`}
-    });
-
-    const data = await result.json();
-    return data.categories.items;
-  };
-
-  return {
-    getToken() {
-      return _getToken();
-    },
-
-    getGenres(token) {
-      return _getGenres(token);
-    }
-  };
-}());
-
 // Leaflet Map w/Heatmap Code
-const testData = {
-  max: 8,
-  data: [{lat: 24.6408, lng: 46.7728, count: 3}, {lat: 50.75, lng: -1.55, count: 1}]
-};
+function initMap() {
+  const testData = {
+    max: 8,
+    data: [{lat: 24.6408, lng: 46.7728, count: 3}, {lat: 50.75, lng: -1.55, count: 1}]
+  };
 
-const baseLayer = tileLayer(
-  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '...',
-    maxZoom: 18
+  const baseLayer = tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '...',
+      maxZoom: 18
+    }
+  );
+
+  const cfg = {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    // if scaleRadius is false it will be the constant radius used in pixels
+    radius: 2,
+    maxOpacity: 0.8,
+    // scales the radius based on map zoom
+    scaleRadius: true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries
+    //   (there will always be a red spot with useLocalExtremas true)
+    useLocalExtrema: true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  };
+  //
+  const heatmapLayer = HeatmapOverlay(cfg);
+
+  const map = new Map('map-canvas', {
+    center: new LatLng(25.6586, -80.3568),
+    zoom: 4,
+    layers: [baseLayer, heatmapLayer]
+  });
+
+  heatmapLayer.setData(testData);
+}
+
+function filterData(array, input) {
+  return array.filter((item) => {
+    if (!item.school_name) { return; }
+    const lowerCase = item.school_name.toLowerCase();
+    const lowerCaseQuery = input.toLowerCase();
+    return lowerCase.includes(lowerCaseQuery);
+  });
+}
+
+function injectHTML() {
+  const target = document.querySelector('#loc_list');
+  target.innerHTML = '';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+  list.forEach((item) => {
+    const el = document.createElement('li');
+    el.innerText = item.school_name;
+    listEl.appendChild(el);
+  })
+}
+
+// Main Function
+async function mainEvent() {
+  const pageMap = initMap();
+
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-loc');
+
+  const results = await fetch('/api/internetPG');
+  const resultsArray = await results.json();
+
+  if (resultsArray.data?.length > 0) {
+    let currentList = [];
+
+    form.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const filteredLocs = filterData(currentList, event.target.value);
+      injectHTML(filteredLocs);
+    })
   }
-);
+}
 
-const cfg = {
-  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-  // if scaleRadius is false it will be the constant radius used in pixels
-  radius: 2,
-  maxOpacity: 0.8,
-  // scales the radius based on map zoom
-  scaleRadius: true,
-  // if set to false the heatmap uses the global maximum for colorization
-  // if activated: uses the data maximum within the current map boundaries
-  //   (there will always be a red spot with useLocalExtremas true)
-  useLocalExtrema: true,
-  // which field name in your data represents the latitude - default "lat"
-  latField: 'lat',
-  // which field name in your data represents the longitude - default "lng"
-  lngField: 'lng',
-  // which field name in your data represents the data value - default "value"
-  valueField: 'count'
-};
-//
-const heatmapLayer = HeatmapOverlay(cfg);
-
-const map = new Map('map-canvas', {
-  center: new LatLng(25.6586, -80.3568),
-  zoom: 4,
-  layers: [baseLayer, heatmapLayer]
-});
-
-heatmapLayer.setData(testData);
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
 
 /*
 // Initial Leaflet Map
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,17 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
-import foodService from './foodService.js';
+import internetService from './internetService.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the UMD Dining API!');
+  res.send('Welcome to the PG County Public Schools Internet API!');
 });
 
 // Generic API inclusion demonstration
 // Replace this with the group member's actual route
 // This leads to /api/member1
-router.use('/foodService', foodService);
+router.use('/internetService', internetService);
 
 export default router;
