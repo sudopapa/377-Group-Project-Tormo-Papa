be5afa30e847a12ad25b08133ca9703055dd240b
ttormo <ttormo@umd.edu>
2022-12-12 15:44:03 -0500

    re-wrote the entire mainEvent function

--- client/charts_script.js
+++ client/charts_script.js
@@ -1,5 +1,6 @@
 // API Init
 const { default: SpotifyWebApi } = require('spotify-web-api-js');
+
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken('BQBZp6lSx_VtsJ3lB7P52raLlzwrskbwumBEsGX-BD4lTCejdGPf9IwCJcJY98UMb2dDxSRudd6LrKd7N1gpQLTKIiT2OLVAkW-dqmjQDDilaVuHWhAFX_us25-8CFN1ivNjkECYFKrsg3JMu5D7TI14H_c1swFPr6muBeXH0oI2UjNIltBDewMF-UlY4aqu10WvBr0');
 
@@ -8,20 +9,20 @@ function spotQuery(artist) {
   spotifyApi.searchArtists(artist);
 }
 
-//Inject HTML
+// Inject HTML
 function injectHTML(list) {
-    console.log('fired injectHTML');
-    const target = document.querySelector('#album_list');
-    target.innerHTML = '';
-  
-    const listEl = document.createElement('ol');
-    target.appendChild(listEl);
-  
-    list.forEach((item) => {
-      const el = document.createElement('li');
-      el.innerText = item.name;
-      listEl.appendChild(el);
-    });
+  console.log('fired injectHTML');
+  const target = document.querySelector('#album_list');
+  target.innerHTML = '';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+
+  list.forEach((item) => {
+    const el = document.createElement('li');
+    el.innerText = item.name;
+    listEl.appendChild(el);
+  });
 }
 
 // Main Function
@@ -31,12 +32,15 @@ async function mainEvent() {
 
   form.addEventListener('input', (event) => {
     console.log('input', event.target.value);
-    form.addEventListener('submit', (submitEvent) => {
-          submitEvent.preventDefault();
-          albumList = spotQuery(event.target.value);
-          injectHTML(albumList);
-    });
-  }
+    const artist = event.target.value;
+  });
+
+  form.addEventListener('submit', (submitEvent) => {
+    submitEvent.preventDefault();
+    albums = spotQuery(artist);
+    console.log(albums);
+    injectHTML(albums);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
