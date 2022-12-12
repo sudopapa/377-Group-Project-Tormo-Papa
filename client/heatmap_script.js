c658b4c4cda93f8a6feb850fb58366d89de37e20
sudopapa <112443607+sudopapa@users.noreply.github.com>
2022-12-11 23:20:11 -0500

    Update heatmap_script.js

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -1,4 +1,4 @@
-var request = require('request');
+import request from 'request';
 
 var accessToken = 'BQACX0bVaTJ99cc7DJwDua1RqDnpj-VXOPacGoI51GypyRWNwCS2wJdlkd--TIFoVXpSNCppN1D_Xv4RvJhu8d9gHzwVOVyuzgdpzaAw96yjWAOTtXtvnQDcj8TrHXq3aIKxbgPSbIdnJJDz74jcb0lNbQztUxoemvnvdyZ0DtwyzO8QAuNXlNiMgfUNaFFm7XU';
   
