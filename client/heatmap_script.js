23dada873b7cf5a84b598392623f01c0d59e4a54
sudopapa <112443607+sudopapa@users.noreply.github.com>
2022-12-12 21:18:35 -0500

    Update heatmap_script.js

--- client/heatmap_script.js
+++ client/heatmap_script.js
@@ -1,5 +1,5 @@
 // Imports
-import HeatmapOverlay from './leaflet-heatmap.js';
+const HeatmapOverlay = require('./leaflet-heatmap.js');
 
 // Filters out items that don't include the input
 function filterData(array, input) {
